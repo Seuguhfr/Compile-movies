0b2ca0953e7172d51383739cdd43b08b43affb45
Seuguhfr <hdbdt1597@gmail.com>
2024-01-03 13:39:27 +0100

    Add movie_hash and movie_name functions

--- Second.py
+++ Second.py
@@ -1,9 +1,11 @@
 import os
 import requests
 from pythonopensubtitles.opensubtitles import OpenSubtitles
+from pythonopensubtitles.utils import File
 import subprocess
 import hashlib
 import omdb
+
 
 def compile(video, subtitles, cover, name, directory) -> None:
     english_subtitles, french_subtitles = subtitles
@@ -32,3 +34,17 @@ def find_video(directory) -> str:
         if file.endswith(".mkv") or file.endswith(".mp4"):
             return file
         
+def movie_hash(file_path):
+    f = File(file_path)
+    return f.get_hash()
+
+def movie_name(video) -> str:
+    os_instance = OpenSubtitles()
+    os_instance.login('hdbdt1597@gmail.com', 'B)#@=IPP17b$};1Xt3ReMY(o')
+    video_hash = movie_hash(video)
+    movie_info = os_instance.search_subtitles([{'sublanguageid': 'eng', 'moviehash': video_hash}])
+    if movie_info:
+        return movie_info[0]['MovieName']
+    else:
+        return None
+
