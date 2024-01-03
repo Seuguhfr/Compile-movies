877bf1f9f2df7f007a8f2e9628a9cbec17473f14
Seuguhfr <hdbdt1597@gmail.com>
2024-01-03 20:36:40 +0100

    Add subtitle download functionality

--- Second.py
+++ Second.py
@@ -1,11 +1,9 @@
 import os
-import requests
-from pythonopensubtitles.opensubtitles import OpenSubtitles
-from pythonopensubtitles.utils import File
+from guessit import guessit
+from babelfish import Language
+from subliminal import download_best_subtitles, save_subtitles, Video, scan_video, scan_videos, download_subtitles
 import subprocess
-import hashlib
 import omdb
-
 
 def compile(video, subtitles, cover, name, directory) -> None:
     english_subtitles, french_subtitles = subtitles
@@ -33,18 +31,28 @@ def find_video(directory) -> str:
     for file in os.listdir(directory):
         if file.endswith(".mkv") or file.endswith(".mp4"):
             return file
-        
-def movie_hash(file_path):
-    f = File(file_path)
-    return f.get_hash()
 
-def movie_name(video) -> str:
-    os_instance = OpenSubtitles()
-    os_instance.login('hdbdt1597@gmail.com', 'B)#@=IPP17b$};1Xt3ReMY(o')
-    video_hash = movie_hash(video)
-    movie_info = os_instance.search_subtitles([{'sublanguageid': 'eng', 'moviehash': video_hash}])
-    if movie_info:
-        return movie_info[0]['MovieName']
-    else:
-        return None
+def movie_name(video_path) -> str:
+    movie_info = guessit(video_path)
+    return movie_info["title"]
+    
 
+def subtitles(video_path: str, languages: list) -> list:
+    languages: set = {Language(language) for language in languages}
+    video = scan_video(video_path)
+    subtitles = download_best_subtitles([video], languages)
+    save_subtitles(video, subtitles[video])
+    subtitles_files: list = []
+    file_name = os.path.splitext(os.path.basename(video_path))[0]
+    for language in languages:
+        subtitles_files.append(f"{file_name}.{language.alpha2}.srt")
+    return subtitles_files
+
+
+# Test
+directory = "C:\\Users\\Hugues\\Downloads\\PopcornTime - Downloads\\La.La.Land.2016.1080p.BluRay.DDP5.1.x265.10bit-GalaxyRG265[TGx]"
+video = find_video(directory)
+video_path = os.path.join(directory, video)
+print(subtitles(video_path, ["eng", "fra"]))
+
+# iso_codes = [lang for lang in pycountry.languages if hasattr(lang, 'alpha_2')]
